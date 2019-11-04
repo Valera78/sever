850b0ca77a7586cfe7b52151ff75e130089fa0bc
unknown <Valera78>
2019-11-04 15:22:26 +0800

    send

--- node.js
+++ node.js
@@ -2,11 +2,15 @@ var http = require("http");
 
 
 
+
+
+
+
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://User:<User1>@users-x07hv.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://:@users-x07hv.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices").insertOne({'HY SHO':"Sobaci"});
+  const collection = client.db("test").collection("devices").insertOne({'f21312312312':'f312321312321321312'});
   console.log('я отправил нахуй');
   client.close();
 });
